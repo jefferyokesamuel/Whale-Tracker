052b979f00081317892b663917b9aac8c89e579f
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2022-09-30 23:27:44 +0100

    changes

---
+++ index.js
@@ -0,0 +1,19 @@
+const ethers = require('ethers')
+
+const rpcURL = "https://cloudflare-eth.com"
+const provider = new ethers.providers.JsonRpcProvider(rpcURL)
+
+const CONTRACT_ADDRESS = "0xdAC17F958D2ee523a2206206994597C13D831ec7"
+const CONTRACT_ABI = 
+
+const contract = new ethers.Contract(CONTRACT_ADDRESS, ,provider)
+
+const main = async () => {
+    xcvcx
+
+    const block = await provider.getBlockNumber()
+    console.log(block)
+    console.log('Program running');
+}
+
+main()
